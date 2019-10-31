04c6f5c111cf48244b729abccaa2fdb0560a43fb
Liviu Preda <liviupreda@gmail.com>
2019-10-31 14:05:41 +0200

    roll dice method

--- src/RollDice.js
+++ src/RollDice.js
@@ -3,15 +3,32 @@ import Die from './Die';
 import './RollDice.css';
 
 class RollDice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      die1: 'one',
+      die2: 'two'
+    };
+  }
+
+  roll = () => {
+    const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
+    let rand1 = sides[Math.floor(Math.random() * 6)],
+      rand2 = sides[Math.floor(Math.random() * 6)];
+    this.setState({ die1: rand1, die2: rand2 });
+  };
+
   render() {
     return (
       <div className='RollDice'>
         <div className='RollDice-Dice'>
-          <Die />
-          <Die />
+          <Die side={this.state.die1} />
+          <Die side={this.state.die2} />
         </div>
         <div>
-          <button className='RollDice-button'>Roll da dice 'mon</button>
+          <button className='RollDice-button' onClick={this.roll}>
+            Roll da dice 'mon
+          </button>
         </div>
       </div>
     );
